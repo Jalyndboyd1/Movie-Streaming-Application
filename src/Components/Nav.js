e9198b5abc33b3af4a46a7a1fabc423f985d45cd
Jalyn.Boyd <Jalyn.Boyd@oneamerica.com>
2022-09-30 14:48:49 -0400

    Created Nav.js transition feature

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,19 @@
 import "./css/Nav.css"
 import logo from "../utils/symbol.png"
+import { useState, useEffect } from "react"
 
 function Nav() {
+    const [showNav, setShowNav] = useState(false)
+    const handleScroll = () => window.scrollY > 100 ? setShowNav(true) : setShowNav(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+        // use effect clean up
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, [showNav])
+
     return (
-        <div className="nav nav__black">
+        <div className={`nav ${showNav && "nav__black"}`}>
             <div class="nav__contents">
                 <img className="nav__logo" src={logo} alt="Movie Streaming Application" />
                 <img className="nav__avatar" src={"https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"} alt="" />
